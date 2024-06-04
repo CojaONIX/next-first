be7e522d617f8239b4b41cdb9de42f7fefdd90a8
CojaONIX <cojaonix@gmail.com>
2024-06-04 11:29:56 +0200

    19.12 allowed days

--- app/weather/day/[dayInAWeek]/page.js
+++ app/weather/day/[dayInAWeek]/page.js
@@ -1,4 +1,14 @@
 
 export default function WeatherDayInAWeek({params}) {
+    const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+    if(!days.includes(params.dayInAWeek)) {
+        return (
+            <>
+            <p>Wrong day!</p>
+            <p>Allowed days: {JSON.stringify(days)}</p>
+            </>
+        )
+    }
     return <p>WeatherDayInAWeek Page - {params.dayInAWeek}</p>
 }
